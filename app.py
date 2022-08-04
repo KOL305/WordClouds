039a29b30405e7ba360c352d90d0480d24ba04b1
Kadon Liang <kadonliang@gmail.com>
2022-08-03 21:59:14 -0700

    added js

--- app.py
+++ app.py
@@ -1,40 +1,50 @@
+import base64
+import io
 from functools import wraps
 from bson.objectid import ObjectId
-import datetime
 from flask import Flask, render_template, jsonify, request, redirect, session, abort, flash
-from flask_minify import minify
-from flask_pymongo import PyMongo
 from flask_session import Session
-from passlib.hash import pbkdf2_sha256
-import pymongo
-from config import Config, db
+from config import Config
+import wordcloud
+import matplotlib
+import pandas
+import numpy
 
 app = Flask("ATT Hackathon Project")
 app.config.from_object(Config)
-client = PyMongo(app)
-db = client.db
 Session(app)
-
-def login_required(something):
-    @wraps(something)
-    def wrap_login(*args, **kwargs):
-        if 'logged_in' in session and session["logged_in"]:
-            return something(session['logged_in_id'], *args, **kwargs)
-        else:
-            flash("Please Sign In First", category="danger")
-            return redirect('/')
-    return wrap_login
 
 @app.route('/', methods=['GET','POST'])
 def home():
     if request.method == 'GET':     
-        if 'logged_in' in session and session["logged_in"]:
-            user = db.users.find_one({'_id': ObjectId(session["logged_in_id"])})
-            bal = user['balance']
-            return render_template('home.html', bal=bal)
         return render_template('home.html')
     elif request.method == 'POST':
+        pass
+
+@app.route('/cloud', methods=['GET','POST'])
+def cloud():
+    if request.method == 'GET':     
         return render_template('home.html')
+    elif request.method == 'POST':
+        text = request.files['text']
+        mask = request.files['mask']
+        color = request.form['color']
+        textFile = open('%s.txt' % text, 'r')
+        print(textFile.read())
+        print("Color",color)
+
+        data = io.BytesIO()
+        base64_string = "data:image/png;base64," + \
+        str(base64.b64encode(mask.read()).decode('utf-8'))
+        return {"filename": base64_string}
+
+@app.route('/cloudexp', methods=['GET','POST'])
+def cloudExp():
+    if request.method == 'GET':     
+        return render_template('home.html')
+    elif request.method == 'POST':
+        pass
+
 
 if __name__ == "__main__":
     app.config['SECRET_KEY'] = '123qwi34iWge9era89F1393h3gwJ0q3'
